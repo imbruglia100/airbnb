04ca5df8b8ed2e5819f03dcad2566d022a3328cd
frank <imbruglia100@gmail.com>
2024-04-15 20:16:29 -0400

    deletes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,9 +3,5 @@ const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
 
-router.get('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-  });
-
 
 module.exports = router;
