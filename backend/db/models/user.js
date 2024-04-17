e5da2c60a2a401c5d3487cd4fc83ccbd37681c1f
frank <imbruglia100@gmail.com>
2024-04-16 21:00:33 -0400

    changes

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -12,14 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
+    
     username: {
       type: DataTypes.STRING,
       allowNull: false,
