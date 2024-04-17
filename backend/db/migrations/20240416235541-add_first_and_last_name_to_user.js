1d0beea664e16a6a666cdb13fe17ba6ec8efcf75
frank <imbruglia100@gmail.com>
2024-04-17 19:00:55 -0400

    f

--- backend/db/migrations/20240416235541-add_first_and_last_name_to_user.js
+++ backend/db/migrations/20240416235541-add_first_and_last_name_to_user.js
@@ -3,12 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
     await queryInterface.addColumn("Users", 'firstName', {
       type: Sequelize.STRING,
       allowNull: false
@@ -21,12 +15,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
     await queryInterface.removeColumn("Users", 'firstName')
     await queryInterface.removeColumn("Users", 'lastName')
   }
